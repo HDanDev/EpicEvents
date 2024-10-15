042483fd2a743646c4a1f0300c08ae4c173708d3
Dan <danhannay@vr-connection.com>
2024-10-15 22:58:28 +0200

    Dev branch creation

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ username = os.getenv('DB_USERNAME')
 password = os.getenv('DB_PASSWORD')
 db_name = os.getenv('DB_NAME')
 db_host = os.getenv('DB_HOST')
-pswd = '"}8*BNF2X"NTF@?p"'
+# pswd = '"}8*BNF2X"NTF@?p"'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{username}:{password}@{db_host}/{db_name}'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://root:@localhost/epic_events'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{username}:{password}@{db_host}/{db_name}'
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://root:@localhost/epic_events'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)

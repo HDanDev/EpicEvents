c528c505c6f88bfe57e1128650c49311145566c3
Dan <72689919+HDanDev@users.noreply.github.com>
2025-02-01 23:23:38 +0100

    Sentry implementation

--- app.py
+++ app.py
@@ -4,6 +4,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import TestConfig, Config
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_URL'),
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    _experiments={
+        # Set continuous_profiling_auto_start to True
+        # to automatically start the profiler on when
+        # possible.
+        "continuous_profiling_auto_start": True,
+    },
+)
 
 load_dotenv()
 
